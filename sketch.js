6a218a99a21a8431a7e6c1dafb05b909ce2614be
MrLagSux <41621734+MrLagSux@users.noreply.github.com>
2019-05-12 19:24:15 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -103,8 +103,14 @@ function draw() {
 
 function keyPressed() {
   setMove(true);
-  return false;
 }
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
 
 function keyReleased() {
   setMove(false);
